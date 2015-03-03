d675195041effbbc1f9b600db1ba85f8b5fd3658
Nikita Cherevkov <nikita.cherevkov@gmail.com>
2015-03-03 13:11:20 -0500

    cleared all to start again

--- modz/mail.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Nikita on 26.12.2014.
- */
-
-var inbox = require('inbox');
-
-module.exports = inbox;
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Dog Log Viewer' });
-});
-
-module.exports = router;
